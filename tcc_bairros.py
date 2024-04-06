35885cfafeca56d4a92b70907c94637002c81014
Felipe Relvas <feliperelvas@id.uff.br>
2024-04-06 17:24:47 -0300

    Commit inicial com o arquivo .py e dados

---
+++ tcc_bairros.py
@@ -0,0 +1,86 @@
+import geopandas as gpd
+import pandas as pd
+from pyproj import Proj, transform
+import folium
+
+niteroi = gpd.read_file(r'Limite_de_Bairros\Limite_de_Bairros.shp')
+
+# Pegando os centroides
+centroide = niteroi.geometry.centroid
+
+# Dando uma olhada nos bairros
+niteroi['tx_nome']
+
+# Criando um DF com os dados: nomes do bairros, coordenada X do centroide e coordenada Y do centroide
+df_niteroi = pd.DataFrame(data=[niteroi['tx_nome'], centroide.x, centroide.y])
+
+# Transpondo o DF para ser melhor visualizado
+df_niteroi = df_niteroi.T
+
+# Alterando o nome das colunas para o DF ficar melhor
+df_niteroi.rename(columns={'tx_nome': 'Bairro', 'Unnamed 0': 'Coordenada X', 'Unnamed 1': 'Coordenada Y'}, inplace=True)
+
+# Setando o index como a coluna do nome do bairro para ficar melhor de utilizar
+df_niteroi = df_niteroi.set_index('Bairro')
+
+
+##########################
+
+# Agora, vamos precisar trazer as medidas dos centroides que estão em metros para coordenadas para usar no mapa (latitude e longitude)
+'''
+from pyproj import Proj, transform
+
+# Coordenadas do centróide em metros (exemplo para Varzea das Moças)
+centroide_x = df_niteroi['Coordenada X']['Varzea das Moças']
+centroide_y = df_niteroi['Coordenada Y']['Varzea das Moças']
+
+# Definir o sistema de coordenadas de origem (UTM) e destino (latitude e longitude)
+in_proj = Proj(proj='utm', zone=23, south=True, ellps='WGS84')  # Zona UTM 23S para Niterói
+out_proj = Proj(proj='latlong', ellps='WGS84')
+
+# Converter coordenadas do centróide de UTM para latitude e longitude
+longitude, latitude = transform(in_proj, out_proj, centroide_x, centroide_y)
+
+# Imprimir as coordenadas em latitude e longitude
+print("Latitude:", latitude)
+print("Longitude:", longitude)
+'''
+# Agora, fazendo isso para todos os bairros e adicionando no DF
+
+# Pegando a lista de bairros
+lista_bairros = df_niteroi.index.values.tolist()
+
+# Definir o sistema de coordenadas de origem (UTM) e destino (latitude e longitude)
+in_proj = Proj(proj='utm', zone=23, south=True, ellps='WGS84')  # Zona UTM 23S para Niterói
+out_proj = Proj(proj='latlong', ellps='WGS84')
+
+# Lista com as latitudes
+lista_latitudes = []
+# Lista com as longitudes
+lista_longitudes = []
+
+# loop para todos bairros
+for bairro in lista_bairros:
+    centroide_x = df_niteroi['Coordenada X'][bairro]
+    centroide_y = df_niteroi['Coordenada Y'][bairro]
+    longitude, latitude = transform(in_proj, out_proj, centroide_x, centroide_y)
+    lista_latitudes.append(latitude)
+    lista_longitudes.append(longitude)
+
+# Adicionando as listas no DF
+df_niteroi['Latitude'] = lista_latitudes
+df_niteroi['Longitude'] = lista_longitudes
+
+
+######################################
+
+# Agora, vamos plotar no folium
+
+mapa = folium.Map(location = [-22.918769490782978, -43.056979733841224], zoom_start = 12, width = 600, height = 400, control_scale = True)
+
+for bairro in lista_bairros:
+    folium.Marker(location = [df_niteroi['Latitude'][bairro], df_niteroi['Longitude'][bairro]],
+                  tooltip = bairro, 
+                  icon = folium.Icon(color = 'black'),).add_to(mapa)
+
+mapa
